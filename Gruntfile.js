14e6c479b19d2662a511cec8cbc9be96d77b955b
Akshay Patel <akshayrpatel@gmail.com>
2013-08-16 19:20:39 -0700

    Run imagemin to kill package size
    Former-commit-id: 5e8ccadf3aa65e177b24f91c118e9bb0d5388f9d

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,19 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc'
             },
             all: ['Gruntfile.js', 'conf/**/*.js', 'public/**/*.js', 'app.js']
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/img/upload',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'public/img/upload'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 };
