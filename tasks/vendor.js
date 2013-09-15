0341faf786fcee7fbd2a0655009ff2c19a67f634
Akshay Patel <akshayrpatel@gmail.com>
2013-09-15 15:17:10 -0700

    Vendor task should look through nested assets
    Former-commit-id: 6d6cc13ab91143b6d5109419dea88bfd405d4a1c

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -1,23 +1,65 @@
 'use strict';
 
 module.exports = function (grunt) {
+
     /*jshint unused: false */
     grunt.registerTask('vendor', 'Grab latest vendor files', function () {
         var opts = grunt.config.get('vendor'),
+            done = this.async(),
+            keys = Object.keys(opts),
             request = require('request'),
             fs = require('fs'),
             url = require('url'),
-            done = this.async();
+            traverse = require('traverse'),
+            numDone = 0,
+            assets, length;
 
-        Object.keys(opts).forEach(function (key) {
-            var val = opts[key],
-                path = url.parse(val).path.split('/'),
+        assets = traverse(opts).reduce(function (acc, x) {
+            if (this.isLeaf) {
+                acc.push(x);
+            }
+
+            return acc;
+        }, []);
+
+        length = assets.length;
+
+        function onReqDone(e) {
+            numDone = numDone + 1;
+
+            if (length === numDone) {
+                done();
+            }
+        }
+
+        function fetchAsset(file, lib, done) {
+            var filepath = getFileName(file, lib),
+                req;
+
+            req = request(file, grunt.log.writeln('Fetching: ' + file)).pipe(fs.createWriteStream(filepath));
+            req.on('finish', onReqDone);
+        }
+
+        function getFileName(file, lib) {
+            var path = url.parse(file).path.split('/'),
                 filename = path[path.length - 1],
-                dirpath = './public/vendor/' + key,
+                dirpath = './public/vendor/' + lib,
                 filepath = dirpath + '/' + filename;
 
             grunt.file.mkdir(dirpath);
-            request(val, grunt.log.writeln('Fetching: ' + val)).pipe(fs.createWriteStream(filepath));
+
+            return filepath;
+        }
+
+
+        keys.forEach(function (key) {
+            var val = opts[key];
+
+            if (val instanceof Array) {
+                val.forEach(function (file) { fetchAsset(file, key, done); });
+            } else {
+                fetchAsset(val, key, done);
+            }
         });
     });
 };
