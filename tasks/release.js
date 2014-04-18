56c7d06eaa89cc54b2dcba30501302c98267ce8b
Akshay Patel <akshayrpatel@gmail.com>
2014-04-17 23:27:03 -0700

    Use mocha and istanbul relative to app
    Former-commit-id: 3b5cd595ac8959ec46b817098fdfaa749d85864c

--- tasks/release.js
+++ tasks/release.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', 'run mocha-test', function () {
         var done = this.async();
-        require('child_process').exec('istanbul cover --print summary _mocha -- -R spec', function (err, stdout) {
+        require('child_process').exec('./node_modules/istanbul/lib/cli.js cover --print summary ./node_modules/mocha/bin/_mocha -- -R spec', function (err, stdout) {
             grunt.log.write(stdout);
             done(err);
         });
