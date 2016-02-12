1b3585b2ce32f00e02dd49de63c0882b56ed825f
Akshay Patel <akshayrpatel@gmail.com>
2016-02-12 10:16:51 -0800

    Make eslint happy

--- public/sandbox/scrollview-pagination/paginator-plus.js
+++ public/sandbox/scrollview-pagination/paginator-plus.js
@@ -1,30 +1,30 @@
-YUI.add('scrollview-paginator-plus', function(Y) {
-
+/* global YUI */
+YUI.add('scrollview-paginator-plus', function (Y) {
 /**
  * Provides a plugin, which adds pagination support to ScrollView instances
  *
  * @module scrollview-paginator
  */
 
-var UI = (Y.ScrollView) ? Y.ScrollView.UI_SRC : "ui",
-    INDEX = "index",
-    SCROLL_X = "scrollX",
-    SCROLL_Y = "scrollY",
-    TOTAL = "total",
-    BOUNDING_BOX = "boundingBox",
-    CONTENT_BOX = "contentBox";
+    var UI = (Y.ScrollView) ? Y.ScrollView.UI_SRC : 'ui';
+    var INDEX = 'index';
+    var SCROLL_X = 'scrollX';
+    var SCROLL_Y = 'scrollY';
+    var TOTAL = 'total';
+    var BOUNDING_BOX = 'boundingBox';
+    var CONTENT_BOX = 'contentBox';
 
 /**
  * Scrollview plugin that adds support for paging
  *
  * @class ScrollViewPaginator
  * @namespace Plugin
- * @extends Plugin.Base 
+ * @extends Plugin.Base
  * @constructor
  */
-function PaginatorPlugin() {
-    PaginatorPlugin.superclass.constructor.apply(this, arguments);
-}
+    function PaginatorPlugin () {
+        PaginatorPlugin.superclass.constructor.apply(this, arguments);
+    }
 
 /**
  * The identity of the plugin
@@ -34,7 +34,7 @@ function PaginatorPlugin() {
  * @default 'paginatorPlugin'
  * @static
  */
-PaginatorPlugin.NAME = 'pluginScrollViewPaginator';
+    PaginatorPlugin.NAME = 'pluginScrollViewPaginator';
 
 /**
  * The namespace on which the plugin will reside
@@ -44,7 +44,7 @@ PaginatorPlugin.NAME = 'pluginScrollViewPaginator';
  * @default 'pages'
  * @static
  */
-PaginatorPlugin.NS = 'pages';
+    PaginatorPlugin.NS = 'pages';
 
 /**
  * The default attribute configuration for the plugin
@@ -53,7 +53,7 @@ PaginatorPlugin.NS = 'pages';
  * @type Object
  * @static
  */
-PaginatorPlugin.ATTRS = {
+    PaginatorPlugin.ATTRS = {
 
     /**
      * CSS selector for a page inside the scrollview. The scrollview
@@ -62,10 +62,10 @@ PaginatorPlugin.ATTRS = {
      * @attribute selector
      * @type {String}
      */
-    selector: {
-        value: null
-    },
-    
+        selector: {
+            value: null
+        },
+
     /**
      * The active page number for a paged scrollview
      *
@@ -73,10 +73,10 @@ PaginatorPlugin.ATTRS = {
      * @type {Number}
      * @default 0
      */
-    index: {
-        value: 0
-    },
-    
+        index: {
+            value: 0
+        },
+
     /**
      * The total number of pages
      *
@@ -84,10 +84,10 @@ PaginatorPlugin.ATTRS = {
      * @type {Number}
      * @default 0
      */
-    total: {
-        value: 0
-    },
-		
+        total: {
+            value: 0
+        },
+
 		/**
      * The total number of items per page
      *
@@ -95,229 +95,234 @@ PaginatorPlugin.ATTRS = {
      * @type {Number}
      * @default 1
      */
-    numItemsPerPage: {
-        value: 1
-    }
-};
+        numItemsPerPage: {
+            value: 1
+        }
+    };
 
-Y.extend(PaginatorPlugin, Y.Plugin.Base, {
+    Y.extend(PaginatorPlugin, Y.Plugin.Base, {
 
     /**
      * Designated initializer
      *
      * @method initializer
+     * @returns {void}
      */
-    initializer: function() {
-        var host,
-            paginator = this; // kweight
-
-        host = paginator._host = paginator.get('host');
-
-        paginator.beforeHostMethod('_flickFrame', paginator._flickFrame);
-        paginator.afterHostMethod('_uiDimensionsChange', paginator._calcOffsets);
-        paginator.afterHostEvent('scrollEnd', paginator._scrollEnded);
-        paginator.afterHostEvent('render', paginator._afterRender);
-
-        paginator.after('indexChange', paginator._afterIndexChange);
-    },
+        initializer: function () {
+            var self = this;
+            self.beforeHostMethod('_flickFrame', self._flickFrame);
+            self.afterHostMethod('_uiDimensionsChange', self._calcOffsets);
+            self.afterHostEvent('scrollEnd', self._scrollEnded);
+            self.afterHostEvent('render', self._afterRender);
+            self.after('indexChange', self._afterIndexChange);
+        },
 
     /**
      * Calculate the page boundary offsets
-     * 
+     *
      * @method _calcOffsets
      * @protected
+     * @returns {void}
      */
-    _calcOffsets : function() {
-        var host = this._host,
-            cb = host.get(CONTENT_BOX),
-            bb = host.get(BOUNDING_BOX),
-            vert = host._scrollsVertical,
-            size = (vert) ? host._scrollHeight : host._scrollWidth,
-            pageSelector = this.get("selector"),
-            pages,
-            offsets;
+        _calcOffsets: function () {
+            var host = this._host;
+            var cb = host.get(CONTENT_BOX);
+            var bb = host.get(BOUNDING_BOX);
+            var vert = host._scrollsVertical;
+            var size = (vert) ? host._scrollHeight : host._scrollWidth;
+            var pageSelector = this.get('selector');
+            var pages;
+            var offsets;
 
-        // Pre-calculate min/max values for each page
-        pages = pageSelector ? cb.all(pageSelector) : cb.get("children");
+            // Pre-calculate min/max values for each page
+            pages = pageSelector ? cb.all(pageSelector) : cb.get('children');
 
-        this.set(TOTAL, pages.size());
+            this.set(TOTAL, pages.size());
 
-        this._pgOff = offsets = pages.get((vert) ? "offsetTop" : "offsetLeft");
-        offsets.push(size - bb.get((vert) ? "offsetHeight" : "offsetWidth"));
-    },
+            this._pgOff = offsets = pages.get((vert) ? 'offsetTop' : 'offsetLeft');
+            offsets.push(size - bb.get((vert) ? 'offsetHeight' : 'offsetWidth'));
+        },
 
     /**
-     * Executed to respond to the flick event, by over-riding the default flickFrame animation. 
+     * Executed to respond to the flick event, by over-riding the default flickFrame animation.
      * This is needed to determine if the next or prev page should be activated.
      *
      * @method _flickFrame
+     * @returns {void}
      * @protected
      */
-    _flickFrame: function() {
-        var host = this._host,
-            velocity = host._currentVelocity,
+        _flickFrame: function () {
+            var host = this._host,
+                velocity = host._currentVelocity,
 
-            inc = velocity < 0,
+                inc = velocity < 0,
 
-            pageIndex = this.get(INDEX),
-            pageCount = this.get(TOTAL);
+                pageIndex = this.get(INDEX),
+                pageCount = this.get(TOTAL);
 
-        if (velocity) {
-
-            if (inc && pageIndex < pageCount - 1) {
-                this.set(INDEX, pageIndex + this.get('numItemsPerPage'));
-            } else if (!inc && pageIndex > 0) {
-                this.set(INDEX, pageIndex - this.get('numItemsPerPage'));
+            if (velocity) {
+                if (inc && pageIndex < pageCount - 1) {
+                    this.set(INDEX, pageIndex + this.get('numItemsPerPage'));
+                } else if (!inc && pageIndex > 0) {
+                    this.set(INDEX, pageIndex - this.get('numItemsPerPage'));
+                }
             }
-        }
 
-        return this._prevent;
-    },
+            return this._prevent;
+        },
 
     /**
      * After host render handler
      *
      * @method _afterRender
+     * @returns {void}
      * @protected
      */
-    _afterRender: function(e) {
-        var host = this._host;
-        host.get("boundingBox").addClass(host.getClassName("paged"));
-    },
+        _afterRender: function () {
+            var host = this._host;
+            host.get('boundingBox').addClass(host.getClassName('paged'));
+        },
 
     /**
      * scrollEnd handler detects if a page needs to change
      *
      * @method _scrollEnded
-     * @param {Event.Facade}
+     * @param {Event.Facade} e Event
+     * @returns {void}
      * @protected
      */
-     _scrollEnded: function(e) {
-         var host = this._host,
-             pageIndex = this.get(INDEX),
-             pageCount = this.get(TOTAL),
-             trans = PaginatorPlugin.SNAP_TO_CURRENT;
+        _scrollEnded: function (e) {
+            var host = this._host,
+                pageIndex = this.get(INDEX),
+                pageCount = this.get(TOTAL),
+                trans = PaginatorPlugin.SNAP_TO_CURRENT;
 
 
-         if(e.onGestureMoveEnd && !host._flicking) {
-             if(host._scrolledHalfway) {
-                 if(host._scrolledForward && pageIndex < pageCount - this.get('numItemsPerPage')) {
-                     this.set(INDEX, pageIndex + this.get('numItemsPerPage'));
-                 } else if (pageIndex > 0) {
-                     this.set(INDEX, pageIndex - this.get('numItemsPerPage'));
-                 } else {
-                     this.snapToCurrent(trans.duration, trans.easing);
-                 }
-             } else {
-                 this.snapToCurrent(trans.duration, trans.easing);
-             }
-         }
+            if (e.onGestureMoveEnd && !host._flicking) {
+                if (host._scrolledHalfway) {
+                    if (host._scrolledForward && pageIndex < pageCount - this.get('numItemsPerPage')) {
+                        this.set(INDEX, pageIndex + this.get('numItemsPerPage'));
+                    } else if (pageIndex > 0) {
+                        this.set(INDEX, pageIndex - this.get('numItemsPerPage'));
+                    } else {
+                        this.snapToCurrent(trans.duration, trans.easing);
+                    }
+                } else {
+                    this.snapToCurrent(trans.duration, trans.easing);
+                }
+            }
 
-         host._flicking = false;
-     },
+            host._flicking = false;
+        },
 
     /**
      * index attr change handler
      *
      * @method _afterIndexChange
+     * @param {Event.Facade} e Event
+     * @returns {void}
      * @protected
      */
-    _afterIndexChange: function(e) {
-        if(e.src !== UI) {
-            this._uiIndex(e.newVal);
-        }
-    },
-    
+        _afterIndexChange: function (e) {
+            if (e.src !== UI) {
+                this._uiIndex(e.newVal);
+            }
+        },
+
     /**
      * Update the UI based on the current page index
      *
      * @method _uiIndex
+     * @param {Number} index Current item index
+     * @returns {void}
      * @protected
      */
-    _uiIndex: function(index) {
-        this.scrollTo(index, 350, 'ease-out');
-    },
+        _uiIndex: function (index) {
+            this.scrollTo(index, 350, 'ease-out');
+        },
 
     /**
      * Scroll to the next page in the scrollview, with animation
      *
      * @method next
+     * @returns {void}
      */
-    next: function() {
-        var index = this.get(INDEX);  
-        if(index < this.get(TOTAL) - this.get('numItemsPerPage')) {
-            this.set(INDEX, index + this.get('numItemsPerPage'));
-        }
-    },
-    
+        next: function () {
+            var index = this.get(INDEX);
+            if (index < this.get(TOTAL) - this.get('numItemsPerPage')) {
+                this.set(INDEX, index + this.get('numItemsPerPage'));
+            }
+        },
+
     /**
      * Scroll to the previous page in the scrollview, with animation
      *
      * @method prev
+     * @returns {void}
      */
-    prev: function() {
-        var index = this.get(INDEX);
-        if(index > 0) {
-            this.set(INDEX, index - this.get('numItemsPerPage'));
-        }
-    },
-    
+        prev: function () {
+            var index = this.get(INDEX);
+            if (index > 0) {
+                this.set(INDEX, index - this.get('numItemsPerPage'));
+            }
+        },
+
     /**
      * Scroll to a given page in the scrollview, with animation.
      *
      * @method scrollTo
-     * @param index {Number} The index of the page to scroll to
-     * @param duration {Number} The number of ms the animation should last
-     * @param easing {String} The timing function to use in the animation
+     * @param {Number} index The index of the page to scroll to
+     * @param {Number} duration The number of ms the animation should last
+     * @param {String} easing The timing function to use in the animation
+     * @returns {void}
      */
-    scrollTo: function(index, duration, easing) {
-        var host = this._host,
-            vert = host._scrollsVertical,
-            scrollAxis = (vert) ? SCROLL_Y : SCROLL_X, 
-            scrollVal = this._pgOff[index];
+        scrollTo: function (index, duration, easing) {
+            var host = this._host,
+                vert = host._scrollsVertical,
+                scrollAxis = (vert) ? SCROLL_Y : SCROLL_X,
+                scrollVal = this._pgOff[index];
 
-        host.set(scrollAxis, scrollVal, {
-            duration: duration,
-            easing: easing
-        });
-    },
-    
+            host.set(scrollAxis, scrollVal, {
+                duration: duration,
+                easing: easing
+            });
+        },
+
     /**
      * Snaps the scrollview to the currently selected page
      *
      * @method snapToCurrent
-     * @param duration {Number} The number of ms the animation should last
-     * @param easing {String} The timing function to use in the animation
+     * @param {Number} duration The number of ms the animation should last
+     * @param {String} easing The timing function to use in the animation
+     * @returns {void}
      */
-    snapToCurrent: function(duration, easing) {
-        var host = this._host,
-            vert = host._scrollsVertical;
+        snapToCurrent: function (duration, easing) {
+            var host = this._host,
+                vert = host._scrollsVertical;
 
-        host._killTimer();
+            host._killTimer();
 
 
-        host.set((vert) ? SCROLL_Y : SCROLL_X, this._pgOff[this.get(INDEX)], {
-            duration: duration,
-            easing: easing
-        });
-    },
+            host.set((vert) ? SCROLL_Y : SCROLL_X, this._pgOff[this.get(INDEX)], {
+                duration: duration,
+                easing: easing
+            });
+        },
 
-    _prevent: new Y.Do.Prevent()
+        _prevent: new Y.Do.Prevent()
 
-});
+    });
 
 /**
- * The default snap to current duration and easing values used on scroll end. 
- * 
+ * The default snap to current duration and easing values used on scroll end.
+ *
  * @property SNAP_TO_CURRENT
  * @static
  */
-PaginatorPlugin.SNAP_TO_CURRENT = {
-    duration : 300,
-    easing : 'ease-out'
-};
+    PaginatorPlugin.SNAP_TO_CURRENT = {
+        duration: 300,
+        easing: 'ease-out'
+    };
 
-Y.namespace('Plugin').ScrollViewPaginator = PaginatorPlugin;
-
-
-}, '3.4.1' ,{requires:['plugin']});
+    Y.namespace('Plugin').ScrollViewPaginator = PaginatorPlugin;
+}, '3.4.1' ,{requires: ['plugin']});
--- public/sandbox/yui-spinner/spinner.js
+++ public/sandbox/yui-spinner/spinner.js
@@ -1,143 +1,136 @@
-YUI.add('spinner', function(Y) {
+/* globals YUI */
+/* eslint no-bitwise: 0 */
 
- var animations = {}, /* Animation rules keyed by their name */
-		div = '<div>',
-		Node = Y.Node;
+YUI.add('spinner', function (Y) {
+    var animations = {}, /* Animation rules keyed by their name */
+        div = '<div>',
+        Node = Y.Node;
 
-var Spinner = Y.Base.create('Spinner', Y.Widget, [], {
-	
-	initializer: function() {
-		this.get('contentBox').setStyles({position: 'relative'}).setAttribute('aria-role', 'progressbar');
-   },
-	
-	render : function() {
-		this._lines();
-	},
-	
-	_lines : function() {
-    var seg,
-				el = this.get('contentBox'),
-				i = 0,
-				pre = this._getVendorPrefix(),
-				color = this.get('color'),
-				length = this.get('length'),
-				lines = this.get('lines'),
-				opacity = this.get('opacity'),
-				radius = this.get('radius'),
-				speed = this.get('speed'),
-				trail = this.get('trail'),
-				width = this.get('width');
+    var Spinner = Y.Base.create('Spinner', Y.Widget, [], {
+        initializer: function () {
+            this.get('contentBox').setStyles({position: 'relative'}).setAttribute('aria-role', 'progressbar');
+        },
+
+        render: function () {
+            this._lines();
+        },
+
+        _lines: function () {
+            var seg,
+                el = this.get('contentBox'),
+                i = 0,
+                pre = this._getVendorPrefix(),
+                color = this.get('color'),
+                length = this.get('length'),
+                lines = this.get('lines'),
+                opacity = this.get('opacity'),
+                radius = this.get('radius'),
+                speed = this.get('speed'),
+                trail = this.get('trail'),
+                width = this.get('width');
 
 
-    for (; i < lines; i++) {
-	
-      seg = Node.create(div).setStyles({
-        position: 'absolute',
-        top: 1+~(width/2) + 'px',
-        transform: 'translate3d(0,0,0)',
-        opacity: opacity
-      });
-			
-			seg.setStyle( pre + 'animation', this._addAnimation(opacity, trail, i, lines) + ' ' + 1/speed + 's linear infinite');
-			seg.appendChild(this._fill(i, length, width, color, lines, radius));
-			el.appendChild(seg);
-    }
-  },
-	
-	_fill : function(i, length, width, color, lines, radius) {
+            for (; i < lines; i++) {
+                seg = Node.create(div).setStyles({
+                    position: 'absolute',
+                    top: 1+~(width/2) + 'px',
+                    transform: 'translate3d(0,0,0)',
+                    opacity: opacity
+                });
 
-		var node = Node.create(div).setStyles({
-			position: 'absolute',
-       width: (length + width) + 'px',
-       height: width + 'px',
-       background: color,
-       boxShadow: '0 0 1px rgba(0,0,0,.1)',
-       transformOrigin: 'left',
-       transform: 'rotate(' + ~~(360/lines * i ) + 'deg) translate(' + radius +'px' +',0)',
-       borderRadius: (width >>1 ) + 'px'
-		});
-		
-     return node;
-   },
-	
-	_getVendorPrefix : function() {
-		var pre;
-		
-		if(Y.UA.webkit) {
-			pre = '-webkit-';
-		}
-		else if(Y.UA.opera) {
-			pre = '-o-';
-		}
-		else if(Y.UA.gecko) {
-			pre = '-moz-';
-		}
-		else {
-			pre = '-ms-';
-		}
-		
-		return pre;
-	},
-	
-	_addAnimation : function(alpha, trail, i, lines) {
-    var name = ['opacity', trail, ~~(alpha*100), i, lines].join('-'),
-        start = 0.01 + i/lines*100,
-        z = Math.max(1-(1-alpha)/trail*(100-start) , alpha),
-				pre = this._getVendorPrefix();
+                seg.setStyle( pre + 'animation',
+                    this._addAnimation(opacity, trail, i, lines) + ' ' + 1/speed + 's linear infinite');
+                seg.appendChild(this._fill(i, length, width, color, lines, radius));
+                el.appendChild(seg);
+            }
+        },
 
-    if (!animations[name]) {
-      var css = 
-				'@' + pre + 'keyframes ' + name + '{' +
-        '0%{opacity:'+z+'}' +
-        start + '%{opacity:'+ alpha + '}' +
-        (start+0.01) + '%{opacity:1}' +
-        (start+trail)%100 + '%{opacity:'+ alpha + '}' +
-        '100%{opacity:'+ z + '}' +
-        '}';
-			Y.StyleSheet(css);
-      animations[name] = 1;
-    }
+        _fill: function (i, length, width, color, lines, radius) {
+            var node = Node.create(div).setStyles({
+                position: 'absolute',
+                width: (length + width) + 'px',
+                height: width + 'px',
+                background: color,
+                boxShadow: '0 0 1px rgba(0,0,0,.1)',
+                transformOrigin: 'left',
+                transform: 'rotate(' + Math.floor((360/lines * i )) + 'deg) translate(' + radius +'px' +',0)',
+                borderRadius: (width >>1 ) + 'px'
+            });
 
-    return name;
-  }
-},
-{
-	ATTRS : {
-		// The number of lines to draw
-		lines : {
-			value : 12
-		},
-		// The length of each line
-		length : {
-			value : 7
-		},
-		// The line thickness
-		width : {
-			value : 4
-		},
-		// The radius of the inner circle
-		radius : {
-			value : 15
-		},
-		// #rgb or #rrggbb
-		color : {
-			value : '#666'
-		},
-		// Rounds per second
-		speed : {
-			value : 1
-		},
-		// Afterglow percentage
-		trail : {
-			value : 100
-		},
-		// Opacity of the trail
-		opacity : {
-			value : 0.25
-		}
-	}
-});
+            return node;
+        },
 
- Y.Spinner = Spinner;
+        _getVendorPrefix: function () {
+            var pre;
 
+            if (Y.UA.webkit) {
+                pre = '-webkit-';
+            } else if (Y.UA.opera) {
+                pre = '-o-';
+            } else if (Y.UA.gecko) {
+                pre = '-moz-';
+            } else {
+                pre = '-ms-';
+            }
+
+            return pre;
+        },
+
+        _addAnimation: function (alpha, trail, i, lines) {
+            var name = ['opacity', trail, Math.floor(alpha*100), i, lines].join('-'),
+                start = 0.01 + i/lines*100,
+                z = Math.max(1-(1-alpha)/trail*(100-start) , alpha),
+                pre = this._getVendorPrefix();
+
+            if (!animations[name]) {
+                var css = '@' + pre + 'keyframes ' + name + '{' +
+                        '0%{opacity:'+z+'}' +
+                        start + '%{opacity:'+ alpha + '}' +
+                        (start+0.01) + '%{opacity:1}' +
+                        (start+trail)%100 + '%{opacity:'+ alpha + '}' +
+                        '100%{opacity:'+ z + '}' + '}';
+                Y.StyleSheet(css);
+                animations[name] = 1;
+            }
+
+            return name;
+        }
+    }, {
+        ATTRS: {
+            // The number of lines to draw
+            lines: {
+                value: 12
+            },
+            // The length of each line
+            length: {
+                value: 7
+            },
+            // The line thickness
+            width: {
+                value: 4
+            },
+            // The radius of the inner circle
+            radius: {
+                value: 15
+            },
+            // #rgb or #rrggbb
+            color: {
+                value: '#666'
+            },
+            // Rounds per second
+            speed: {
+                value: 1
+            },
+            // Afterglow percentage
+            trail: {
+                value: 100
+            },
+            // Opacity of the trail
+            opacity: {
+                value: 0.25
+            }
+        }
+    });
+
+    Y.Spinner = Spinner;
 }, '0.0.1' , { requires: ['node', 'event', 'stylesheet', 'widget', 'base'] });
